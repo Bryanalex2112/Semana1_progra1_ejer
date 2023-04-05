f69dd8820dee30ee326103f32fd7926de83a1e39
Bryanalex2112 <bryanalvarado966@gmail.com>
2023-04-05 12:28:08 -0600

    proyecto

---
+++ semana1ejer/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//creacion de objetos
+using semana1ejer.entidades;
+using semana1ejer.negocio;
+
+Operaciones operacione = new Operaciones();
+ClsOperaciones clsOperaciones = new ClsOperaciones();
+
+//ToInt32 sirve para convertir datos de cadena de caracteres tipo string, a datos enteros tipo int
+
+Console.WriteLine("ingresa el primer dato");
+operacione.Dato1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo dato");
+operacione.dato2 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("el esultado de la opeacion es");
+Console.WriteLine(clsOperaciones.Suma(operacione));
---
+++ semana1ejer/negocio/ClsOperaciones.cs
@@ -0,0 +1,11 @@
+using semana1ejer.entidades;
+
+namespace semana1ejer.negocio
+{
+    public class ClsOperaciones
+    {
+        public int Suma(Operaciones operaciones){
+            return operaciones.resultado = operaciones.Dato1 + operaciones.dato2; 
+        }
+    }
+}
